9f0826c911eea9d57c51827023254fc831d1320e
Puck-The-Bard <zdmcguire1@buffs.wtamu.edu>
2019-11-10 23:45:53 -0600

    Part 4 complete

---
+++ ToH_ZM/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'All Might' },
+  { id: 12, name: 'Endevor' },
+  { id: 13, name: 'Todoroki' },
+  { id: 14, name: 'Deku' },
+  { id: 15, name: 'No. 13' },
+  { id: 16, name: 'Gran Torio' },
+  { id: 17, name: 'Midnight' },
+  { id: 18, name: 'Snipe' },
+  { id: 19, name: 'Hound Dog' },
+  { id: 20, name: 'Eraser Head' }
+];
